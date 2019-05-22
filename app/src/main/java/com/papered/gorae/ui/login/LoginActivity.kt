672dac6226dfeec36682a1dc28e1cb37c6ecef3a
LeeJongHyun <dwg7691@gmail.com>
2019-05-22 20:16:06 +0900

    Add LoginActivity

---
+++ app/src/main/java/com/papered/gorae/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.papered.gorae.ui.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.papered.gorae.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
