b3f2877f45370a90db55d1df5c651f0b5ee927cf
eggbee <ty39563956@gmail.com>
2019-05-23 09:31:45 +0900

    [ADD] MainActivity NavigationDrawer

---
+++ app/src/main/java/com/papered/gorae/ui/main/coupon/CouponViewModel.kt
@@ -0,0 +1,7 @@
+package com.papered.gorae.ui.main.coupon
+
+import android.arch.lifecycle.ViewModel;
+
+class CouponViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/papered/gorae/ui/main/map/MapViewModel.kt
@@ -0,0 +1,7 @@
+package com.papered.gorae.ui.main.map
+
+import android.arch.lifecycle.ViewModel;
+
+class MapViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
